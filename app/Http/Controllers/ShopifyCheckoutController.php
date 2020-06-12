fb3ead1eb9be65eb3faa3f8ba879c43e89bd73b1
Angel Gonzalez <angel@capeandbay.com>
2020-06-12 04:53:19 -0400

    oh derp

--- app/Http/Controllers/ShopifyCheckoutController.php
+++ app/Http/Controllers/ShopifyCheckoutController.php
@@ -21,8 +21,10 @@ class ShopifyCheckoutController extends Controller
         $url = 'https://'.$data['shop'].'/admin/api/2020-04/carts/'.$token.'.json';
         $response = Curl::to($url)
             ->withHeader('X-Shopify-Access-Token: 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')
+            ->asJson(true)
             ->get();
 
+        echo 'AllCommerce presents - a future checkout page with this cart!';
         var_dump($response);
     }
 }
